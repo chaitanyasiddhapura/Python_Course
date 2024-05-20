959de34afe7b6e51d736e6f8434529490269a357
Chaitanya Siddhapura <chaitanyasiddhapura06@gmail.com>
2024-05-20 14:12:46 +0530

    DataType & Variable Example

---
+++ 01_Variables_DataTypes/variables&datatype.py
@@ -0,0 +1,39 @@
+# Variable Declaration
+name = "Chaitanya Siddhapura"
+number = 123456789
+pointNumber = 25.5
+value = True
+a = None 
+
+# Print the variable content 
+print("variable content ->")
+print(name)
+print(number)
+print(pointNumber)
+print(value)
+print(a)
+
+# Print the variable Datatype or return type
+print("variable return type ->")
+print(type(name))
+print(type(number))
+print(type(pointNumber))
+print(type(value))
+print(type(a))
+
+# Q1. Print Sum of number
+print("Sum Of Two Numbers: ")
+
+# Variable
+number1 = 10 
+number2 = 20
+
+# main Logic of addition
+sum = number1 + number2 
+
+# Print the output
+print(sum)
+
+
+
+
