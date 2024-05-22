90ca822adc4a9ec7e16b7b59bbbb6589613a366c
Chaitanya Siddhapura <chaitanyasiddhapura06@gmail.com>
2024-05-22 07:28:25 +0530

    How to get input from the user and Comment in python

---
+++ 01_Variables_DataTypes/Comments&Inputs.py
@@ -0,0 +1,29 @@
+# Sigle line comment 
+
+''''
+This a 
+Multiline Comment
+in python 
+'''
+#print("Hello User")
+print("Hello User")
+
+''''
+Input In Python
+'''
+# Taking input from user and print it 
+
+# String input 
+name = input("Name: ")
+
+# int input 
+number = int(input("Enter a Number: "))
+
+# float input 
+FloatNumber = float(input("Enter a floating point number: "))
+
+
+# Print the information taken from the user
+print("Name: ", name)
+print("Number:", number)
+print("Float Number:", FloatNumber)
