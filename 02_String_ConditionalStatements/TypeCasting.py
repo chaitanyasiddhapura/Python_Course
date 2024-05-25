c9a85e749f5ba97cbecc8df70c74fb6b8d7ee421
Chaitanya Siddhapura <chaitanyasiddhapura06@gmail.com>
2024-05-25 08:57:54 +0530

    Type casting in Python is converting a variable from one data type to another using functions like int(), float(), and str().

---
+++ 02_String_ConditionalStatements/TypeCasting.py
@@ -0,0 +1,23 @@
+# Typecasting is used to convert one data type to another data type.
+
+# String to int
+number1 = "123"
+number1 = int(number1) # type conversion
+print("Datatype and Number: ", type(number1), number1) # print Datatype and number 
+
+
+# int to String
+number3 = "656613"
+number3 = str(number3) 
+print("Datatype and Number: ", type(number3), number3)
+
+# String to Float 
+number2 = "10.12"
+number2 = float(number2) 
+print("Datatype and Number: ", type(number2), number2)
+
+
+# Float to String 
+number4 = "532.16"
+number4 = str(number4) 
+print("Datatype and Number: ", type(number4), number4)
