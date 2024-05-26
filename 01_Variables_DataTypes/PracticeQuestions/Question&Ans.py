93b7f819bed0ebc7dde681d105eb503d7cc34865
Chaitanya Siddhapura <chaitanyasiddhapura06@gmail.com>
2024-05-26 08:03:46 +0530

    Practice Questions

---
+++ 01_Variables_DataTypes/PracticeQuestions/Question&Ans.py
@@ -0,0 +1,17 @@
+'''
+Q1. Write a program to get the value from the user and print there information
+    NAME, NUMBER, AGE, COLLEGE-NAME, etc. 
+'''
+
+# Get input from the user 
+name = input("Enter your name: ")
+Number = input("Enter your number: ")
+age = input("Ennter Your age: ")
+college_name = input("Enter Your college name: ")
+
+# Print there information 
+print("User Info ")
+print("Name: ", name )
+print("Number: ", Number)
+print("Age: ", age)
+print("College name: ",college_name )
