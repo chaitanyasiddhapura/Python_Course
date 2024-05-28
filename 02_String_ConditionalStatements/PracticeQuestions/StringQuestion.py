fbf0f30856f0ca83175023b266f3d0d08cb4d843
Chaitanya Siddhapura <chaitanyasiddhapura06@gmail.com>
2024-05-29 03:53:33 +0530

    Question of string

---
+++ 02_String_ConditionalStatements/PracticeQuestions/StringQuestion.py
@@ -0,0 +1,14 @@
+# Q1. Write a program to input user's first name & print it's length.
+
+# Input 
+Firstname = input("Enter Your First Name: ") 
+
+print("Your Name is: ",Firstname)
+print("Your Name Length is: ",len(Firstname))
+
+
+
+# Q2. Write a program to find occurrence of '$' in a string 
+
+string = "I need some dollars ($). The Symbol of doller is $. "
+print("$ Occurrence is: ",string.count("$"))
