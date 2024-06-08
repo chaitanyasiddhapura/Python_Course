7e61e4879a9af6232ebad2e747b5a56556192567
Chaitanya Siddhapura <chaitanyasiddhapura06@gmail.com>
2024-06-08 16:59:52 +0530

    Loop are used to repeat instruction

---
+++ 05_Loops/whileLoop.py
@@ -0,0 +1,30 @@
+'''
+Loop are used to repeat instruction
+'''
+
+# while loop
+count = 1
+
+while count <= 5:
+    print("Helllo ",count) #print hello 5 times
+    count = count + 1
+
+print("5 - times")
+ 
+
+# print 1 to 9 number 
+
+count = 1 
+while count <= 9 :
+    print(count)
+    count = count + 1
+print("1 to 9") 
+
+
+# print into reverse order 
+count = 5
+while count >= 1:
+    print(count)
+    count = count - 1
+print("reverse order ") 
+
