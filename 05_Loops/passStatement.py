c8bafdbaebae8abc462ddb6206870d85a1ea8270
Chaitanya Siddhapura <chaitanyasiddhapura06@gmail.com>
2024-06-15 14:21:40 +0530

    Pass is a null statement that does nothing. it is used as a placeholder for future code.

---
+++ 05_Loops/passStatement.py
@@ -0,0 +1,12 @@
+'''
+Pass is a null statement that does nothing. it is used as a placeholder for future code.
+'''
+
+for i in range(5):
+    pass # This loop does nothing, often used as a placeholder
+
+# The 'pass' statement can also be used in if-else statements, typically in cases where you want to handle exceptions
+if(i < 5):
+    pass
+
+print("Some Work")
