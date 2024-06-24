c6ed30c9886fe290948177f30371286a3bbded23
Chaitanya Siddhapura <chaitanyasiddhapura06@gmail.com>
2024-06-24 21:50:05 +0530

    Deleting a file using the os module

---
+++ 07_File&IO/DeleteFile.py
@@ -0,0 +1,9 @@
+'''
+Deleting a file using the os module
+
+Module (like a code library) is a file written by anthor programmer that generally has a function we can use. 
+
+'''
+
+import os
+os.remove('07_File&IO/demo.txt')
