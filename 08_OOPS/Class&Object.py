57f9a3a63d779ba39014b1055e46edd384ca2361
Chaitanya Siddhapura <chaitanyasiddhapura06@gmail.com>
2024-06-26 12:26:05 +0530

    A class is a blueprint for creating objects, and an object is an instance of a class with specific values for its attributes.

---
+++ 08_OOPS/Class&Object.py
@@ -0,0 +1,17 @@
+'''
+Class is a blueprint for creating objects.
+'''
+
+# Creating a class
+class Student:
+    name = "Chaitanya"
+
+
+# Creating object(instance)
+s1 = Student()
+print(s1.name)
+
+print(s1)
+print(type(s1))
+
+
