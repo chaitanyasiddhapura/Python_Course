73e63594f3418ee57493a80966297ae1aff3813e
Chaitanya Siddhapura <chaitanyasiddhapura06@gmail.com>
2024-09-02 22:48:11 +0530

    Check if a number is even or odd

---
+++ Logic Building/Basic Questions/Normal/EvenOrOdd.py
@@ -0,0 +1,11 @@
+'''
+Check if a number is even or odd
+'''
+
+num = int(input("Enter a number:")) # User input
+
+# Logic 
+if(num % 2 == 0):
+    print("It's Even number")
+else:
+    print("It's odd number")
